50a53313b65b318ad171f4f96ac45c187cbe6d04
Shawon Ahmed Swagoto <49208428+SASwagoto@users.noreply.github.com>
2023-10-17 15:08:02 +0600

    Add files via upload

---
+++ app/Http/Controllers/RoleController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Spatie\Permission\Models\Role;
+
+class RoleController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $roles = Role::all();
+        $users = User::all();
+        return view('settings.role.role', compact('roles','users'));
+    }
+
+    public function add_role()
+    {
+        $roles = Role::all();
+        return view('settings.role.add_role', compact('roles'));
+    }
+
+    public function create_role(Request $request){
+        
+
+        Role::create(['name' => $request->role_name]);
+
+        return redirect()->route('roles.add');
+
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/TemporaryController.php
@@ -0,0 +1,199 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Spatie\Permission\Models\Role;
+
+class TemporaryController extends Controller
+{
+    public function dept_index()
+    {
+        return view('department.list');
+    }
+
+    public function position_index()
+    {
+        return view('department.position');
+    }
+
+    public function teams_index()
+    {
+        return view('department.teams');
+    }
+
+    public function setting_index()
+    {
+        return view('settings.site');
+    }
+
+    public function notice_index()
+    {
+        return view('notice.add');
+    }
+
+    public function notice_list()
+    {
+        return view('notice.list');
+    }
+
+    public function attendance_list()
+    {
+        return view('attendance.list');
+    }
+
+    public function leave_list()
+    {
+        return view('leaves.list');
+    }
+
+    public function shift_index()
+    {
+        return view('attendance.shift');
+    }
+
+    public function schedule_index()
+    {
+        return view('attendance.schedule');
+    }
+
+    public function holiday_add()
+    {
+        return view('holiday.add');
+    }
+
+    public function leave_add()
+    {
+        return view('leaves.add');
+    }
+
+    public function leave_balance()
+    {
+        return view('leaves.balance');
+    }
+
+    public function leave_request()
+    {
+        return view('leaves.request');
+    }
+
+    public function leave_add_req()
+    {
+        return view('leaves.add_req');
+    }
+
+    public function acc_income()
+    {
+        return view('accounts.income');
+    }
+
+    public function acc_expense()
+    {
+        return view('accounts.expense');
+    }
+
+    public function acc_balance()
+    {
+        return view('accounts.balance');
+    }
+
+    public function rec_post()
+    {
+        return view('recruitment.post');
+
+    }
+
+    public function rec_app()
+    {
+        return view('recruitment.applicants');
+
+    }
+
+    public function rec_int()
+    {
+        return view('recruitment.interview');
+
+    }
+
+    public function rec_on()
+    {
+        return view('recruitment.onboarding');
+
+    }
+
+    public function documents_doc()
+    {
+        return view('documents.doc');
+
+    }
+
+    public function documents_policie()
+    {
+        return view('documents.policie');
+
+    }
+
+    public function documents_add()
+    {
+        return view('documents.add');
+
+    }
+
+    public function payroll_info()
+    {
+        return view('payroll.info');
+
+    }
+
+    public function payroll_advance()
+    {
+        return view('payroll.advance');
+
+    }
+
+    public function payroll_slip()
+    {
+        return view('payroll.slip');
+
+    }
+
+    public function payroll_fund()
+    {
+        return view('payroll.fund');
+
+    }
+
+    public function payroll_tax()
+    {
+        return view('payroll.tax');
+
+    }
+
+    public function payroll_list()
+    {
+        return view('payroll.list');
+
+    }
+
+    public function review_list()
+    {
+        return view('review.list');
+    }
+
+    public function review_add()
+    {
+        return view('review.add');
+    }
+
+    public function review_target()
+    {
+        return view('review.target');
+    }
+
+    public function review_training()
+    {
+        return view('review.training');
+    }
+}
