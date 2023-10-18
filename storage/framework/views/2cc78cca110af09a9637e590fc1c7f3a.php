e2bd4aa057236d9391414937c563f9ff6b92fee4
SASwagoto <saswagoto@gmail.com>
2023-10-18 19:54:18 +0600

    Update Composer

--- storage/framework/views/2cc78cca110af09a9637e590fc1c7f3a.php
+++
@@ -1,5 +0,0 @@
-<div class="footer">
-    <div class="copyright">
-        <p>Copyright &copy; Designed &amp; Developed by <a href="<?php echo e($siteInfo->footer_url); ?>" target="_blank"><?php echo e($siteInfo->footer_title); ?></a> <?php echo e(date('Y')); ?></p>
-    </div>
-</div><?php /**PATH E:\laragon\www\hrms\resources\views/layouts/partials/footer.blade.php ENDPATH**/ ?>
