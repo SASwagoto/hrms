10970aaefc05bcb16fecf0582a0dd81f998e3ece
SASwagoto <saswagoto@gmail.com>
2023-10-18 12:03:28 +0600

    update

--- storage/framework/views/2c57745211ff5fded78dcb1b6471f401.php
+++
@@ -1,5 +0,0 @@
-<div class="footer">
-    <div class="copyright">
-        <p>Copyright © Designed &amp; Developed by <a href="https://smarttechnology.com.bd/" target="_blank">Smart Technology</a> 2023</p>
-    </div>
-</div><?php /**PATH E:\laragon\www\HRMSS\resources\views/layouts/partials/footer.blade.php ENDPATH**/ ?>
--- storage/framework/views/2cc78cca110af09a9637e590fc1c7f3a.php
+++
@@ -1,5 +0,0 @@
-<div class="footer">
-    <div class="copyright">
-        <p>Copyright © Designed &amp; Developed by <a href="https://smarttechnology.com.bd/" target="_blank">Smart Technology</a> 2023</p>
-    </div>
-</div><?php /**PATH E:\laragon\www\hrms\resources\views/layouts/partials/footer.blade.php ENDPATH**/ ?>
